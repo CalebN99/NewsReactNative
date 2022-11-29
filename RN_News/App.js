9fc09fc085ccf817604e85a3fff221f6b9a4018b
Caleb Norris <paintballerz15@gmail.com>
2022-11-28 22:02:10 -0800

    ?

--- RN_News/App.js
+++ RN_News/App.js
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import Home from "./components/Home"
 
 export default function App() {
